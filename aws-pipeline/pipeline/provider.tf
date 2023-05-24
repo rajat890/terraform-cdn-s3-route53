ddd0a2f7f5d59850f337069917373f3e9374e422
root <root@ip-172-31-39-164.ap-south-1.compute.internal>
2023-05-24 11:42:26 +0000

    tfsec addition

--- aws-pipeline/pipeline/provider.tf
+++ aws-pipeline/pipeline/provider.tf
@@ -2,3 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+#terraform {
+#  backend "s3" {
+#    bucket = "terra-tf-backup"
+#    key    = "test-path/terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
